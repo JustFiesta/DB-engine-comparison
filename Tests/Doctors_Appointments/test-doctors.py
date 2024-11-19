320e3b87ad4b71715811168389516461daa83034
bieniekPc <bieniek@pc.com>
2024-11-19 10:31:15 +0100

    add full queries to test-doctors

--- Tests/Doctors_Appointments/test-doctors.py
+++ Tests/Doctors_Appointments/test-doctors.py
@@ -134,12 +134,64 @@ def test_database_performance():
             WHERE 
                 d.doctor_id = 6970;
             """,
-            "",
-            "",
+            """SELECT 
+                a.appointment_date,
+                d.first_name as doctor_first_name,
+                d.last_name as doctor_last_name,
+                p.first_name as patient_first_name,
+                p.last_name as patient_last_name,
+                a.diagnosis
+            FROM Appointments a
+            JOIN Doctors d ON a.doctor_id = d.doctor_id
+            JOIN Patients p ON a.patient_id = p.patient_id
+            LIMIT 10;""",
+            """SELECT 
+                p.first_name,
+                p.last_name,
+                COUNT(*) as total_appointments
+            FROM Appointments a
+            JOIN Patients p ON a.patient_id = p.patient_id
+            GROUP BY a.patient_id, p.first_name, p.last_name
+            HAVING COUNT(*) > 7
+            LIMIT 10;""",
             # podzapytania
-            "",
-            "",
-            ""
+            """WITH DoctorWithMostPatients AS (
+                SELECT 
+                    doctor_id,
+                    COUNT(*) as patient_count
+                FROM Appointments
+                GROUP BY doctor_id
+                ORDER BY COUNT(*) DESC
+                LIMIT 1
+            )
+            SELECT DISTINCT
+                p.first_name,
+                p.last_name
+            FROM Appointments a
+            JOIN Patients p ON a.patient_id = p.patient_id
+            JOIN DoctorWithMostPatients d ON a.doctor_id = d.doctor_id
+            LIMIT 10;
+            """,
+            """SELECT first_name, last_name
+            FROM Patients
+            WHERE patient_id IN (
+                SELECT patient_id
+                FROM Appointments
+                GROUP BY patient_id, diagnosis
+                HAVING COUNT(appointment_id) >= 2
+            );
+            """,
+            """SELECT DISTINCT 
+                first_name, 
+                last_name
+            FROM Patients
+            WHERE patient_id IN (
+                SELECT a.patient_id
+                FROM Appointments a
+                JOIN Doctors d ON a.doctor_id = d.doctor_id
+                WHERE d.specialization = 'Cardiology'
+            )
+            LIMIT 10;"""
         ],
         'MongoDB': [
             # zapytania
@@ -320,42 +372,187 @@ def test_database_performance():
             },
             {
                 'collection': 'Appointments',
-                'query': None,
                 'pipeline': [
-                    
+                    {'$lookup': {
+                        'from': 'Doctors',
+                        'localField': 'doctor_id',
+                        'foreignField': 'doctor_id',
+                        'as': 'doctor'
+                    }},
+                    {'$lookup': {
+                        'from': 'Patients',
+                        'localField': 'patient_id',
+                        'foreignField': 'patient_id',
+                        'as': 'patient'
+                    }},
+                    {'$unwind': '$doctor'},
+                    {'$unwind': '$patient'},
+                    {'$project': {
+                        'appointment_date': 1,
+                        'doctor_first_name': '$doctor.first_name',
+                        'doctor_last_name': '$doctor.last_name',
+                        'patient_first_name': '$patient.first_name',
+                        'patient_last_name': '$patient.last_name',
+                        'diagnosis': 1
+                    }},
+                    {'$limit': 10}
                 ],
                 'projection': None
             },
             {
                 'collection': 'Appointments',
-                'query': None,
                 'pipeline': [
-                    
+                    {'$group': {
+                        '_id': '$patient_id',
+                        'total_appointments': {'$sum': 1}
+                    }},
+                    {'$match': {'total_appointments': {'$gt': 7}}},
+                    {'$lookup': {
+                        'from': 'Patients',
+                        'localField': '_id',
+                        'foreignField': 'patient_id',
+                        'as': 'patient_info'
+                    }},
+                    {'$unwind': '$patient_info'},
+                    {'$project': {
+                        'first_name': '$patient_info.first_name',
+                        'last_name': '$patient_info.last_name',
+                        'total_appointments': 1
+                    }},
+                    {'$limit': 10}
                 ],
                 'projection': None
             },
             # podzapytania
             {
                 'collection': 'Appointments',
-                'query': None,
                 'pipeline': [
-                    
+                    {'$group': {
+                        '_id': '$doctor_id',
+                        'patient_count': {'$sum': 1}
+                    }},
+                    {'$sort': {'patient_count': -1}},
+                    {'$limit': 1},
+                    {'$lookup': {
+                        'from': 'Appointments',
+                        'let': {'doctor_id': '$_id'},
+                        'pipeline': [
+                            {'$match': {
+                                '$expr': {'$eq': ['$doctor_id', '$$doctor_id']}
+                            }},
+                            {'$lookup': {
+                                'from': 'Patients',
+                                'localField': 'patient_id',
+                                'foreignField': 'patient_id',
+                                'as': 'patient'
+                            }},
+                            {'$unwind': '$patient'},
+                            {'$group': {
+                                '_id': {
+                                    'patient_id': '$patient_id',
+                                    'first_name': '$patient.first_name',
+                                    'last_name': '$patient.last_name'
+                                }
+                            }},
+                            {'$project': {
+                                'first_name': '$_id.first_name',
+                                'last_name': '$_id.last_name',
+                                '_id': 0
+                            }},
+                            {'$limit': 10}
+                        ],
+                        'as': 'patients'
+                    }},
+                    {'$unwind': '$patients'},
+                    {'$replaceRoot': {'newRoot': '$patients'}
+                                         },
+                    {'$limit': 10}
                 ],
                 'projection': None
             },
             {
-                'collection': '',
-                'query': None,
+                'collection': 'Appointments',
                 'pipeline': [
-                    
+                    {
+                        '$group': {
+                            '_id': {
+                                'patient_id': '$patient_id',
+                                'diagnosis': '$diagnosis'
+                            },
+                            'count': {'$sum': 1}
+                        }
+                    },
+                    {
+                        '$match': {
+                            'count': {'$gte': 2}
+                        }
+                    },
+                    {
+                        '$lookup': {
+                            'from': 'Patients',
+                            'localField': '_id.patient_id',
+                            'foreignField': 'patient_id',
+                            'as': 'patient_info'
+                        }
+                    },
+                    {
+                        '$unwind': '$patient_info'
+                    },
+                    {
+                        '$project': {
+                            '_id': 0,
+                            'first_name': '$patient_info.first_name',
+                            'last_name': '$patient_info.last_name'
+                        }
+                    }
                 ],
                 'projection': None
             },
             {
-                'collection': '',
-                'query': None,
+                'collection': 'Appointments',
                 'pipeline': [
-                     
+                    {
+                        '$lookup': {
+                            'from': 'Doctors',
+                            'localField': 'doctor_id',
+                            'foreignField': 'doctor_id',
+                            'as': 'doctor'
+                        }
+                    },
+                    {
+                        '$match': {
+                            'doctor.specialization': 'Cardiology'
+                        }
+                    },
+                    {
+                        '$lookup': {
+                            'from': 'Patients',
+                            'localField': 'patient_id',
+                            'foreignField': 'patient_id',
+                            'as': 'patient'
+                        }
+                    },
+                    {
+                        '$unwind': '$patient'
+                    },
+                    {
+                        '$group': {
+                            '_id': {
+                                'first_name': '$patient.first_name',
+                                'last_name': '$patient.last_name'
+                            }
+                        }
+                    },
+                    {
+                        '$project': {
+                            '_id': 0,
+                            'first_name': '$_id.first_name',
+                            'last_name': '$_id.last_name'
+                        }
+                    },
+                    {
+                        '$limit': 10
+                    }
                 ],
                 'projection': None
             },
